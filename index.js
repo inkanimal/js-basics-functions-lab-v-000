421fe6cb2cd4c1a901021d70caa0a691852cab4a
geoffrey woodcock <theatomproject@gmail.com>
2019-03-28 19:08:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blockNumber) {
+  if  (blockNumber > 42) {  
+    return blockNumber - 42 
+  } 
+  if (blockNumber < 42){  
+    return 42 - blockNumber }
+}
+
+
+
+function distanceFromHqInFeet (){
+  
+  
+}
+
+function distanceTraveledInFeet (){
+  
+  
+}
+
+function calculatesFarePrice (){
+  
+  
+}
