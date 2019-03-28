da43d6ca138bdb3b73a513fad221622886adb362
geoffrey woodcock <theatomproject@gmail.com>
2019-03-28 19:31:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,16 +19,18 @@ function distanceTravelledInFeet (start, destination){
   }
 }
 
-function calculatesFarePrice (distanceTravelledInFeet){
-  if (distanceTravelledInFeet > 400 && distanceTravelledInFeet < 2000) {
-  return distanceTravelledInFeet * .02;
-  } elsif {
-    (distanceTravelledInFeet > 2000 && distanceTravelledInFeet < 2500){
-      return 
-    }
-      
-    }
+function calculatesFarePrice (start, destination){
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance >= 400 && distance <= 2000) {
+    return .02 * ( distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
     
   
   
-}
